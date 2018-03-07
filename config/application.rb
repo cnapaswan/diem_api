db0ca31f1d6cfeaa671f34387317967a203443ef
mia <c.napaswan@gmail.com>
2018-03-07 22:04:58 +1100

    add time zone config

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module DiemApi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = 'Melbourne'
+    config.active_record.default_timezone = :local
   end
 end
